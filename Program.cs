6439005e94bad2a4d79f198063c71fde4bfa9174
Edward <Edward@DESKTOP-09V4OT2>
2020-02-20 03:41:44 +0000

    Fixed parsing file size independent of buffer size.

--- Program.cs
+++ Program.cs
@@ -8,38 +8,50 @@ namespace Client
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
             try
             {
                 TcpClient tcpClient = new TcpClient();
-                Console.WriteLine("Connecting.....");
+
+                Console.WriteLine("Connecting to server...");
 
                 tcpClient.Connect("192.168.1.46", 8001);
 
-                Console.WriteLine("Connected");
+                Console.WriteLine("Client running on: " + tcpClient.Client.LocalEndPoint);
+                Console.WriteLine("Connected to: " + tcpClient.Client.RemoteEndPoint);
 
-                string fileName = @"C:\Users\Edward\Desktop\alfie.jpg";
-                tcpClient.Client.SendFile(fileName);
+                string filePath = @"C:\Users\Edward\Desktop\alfie.jpg";
 
+                // Filename [1]
+                string fileName = Path.GetFileName(filePath);
+                byte[] fileNameBytes = Encoding.ASCII.GetBytes(fileName);
+                Console.WriteLine("\nFile name {0}", fileName);
+
+                // Filename length [0]
+                int fileNameLength = fileName.Length;
+                byte fileNameLengthByte = Convert.ToByte(fileNameLength);
+
+                // File [n]
+                byte[] fileBytes = File.ReadAllBytes(filePath);
+                Console.WriteLine("\nFile size {0}", fileBytes.Length);
+
+                // Combined bytes
+                byte[] combinedBytes = AddByteToArray(Combine(fileNameBytes, fileBytes), fileNameLengthByte);
+
+                // Send combined bytes
                 NetworkStream networkStream = tcpClient.GetStream();
-
-                networkStream = tcpClient.GetStream();
-
-                ASCIIEncoding asen = new ASCIIEncoding();
-
-                byte[] response = new byte[tcpClient.ReceiveBufferSize];
-                networkStream.Read(response, 0, tcpClient.ReceiveBufferSize);
-
-               String returnData = Encoding.UTF8.GetString(response);
-              Console.WriteLine("Server Response " + returnData);
+                networkStream.Write(combinedBytes, 0, combinedBytes.Length);
+                Console.WriteLine("\nTotal bytes sent {0}", combinedBytes.Length);
 
                 tcpClient.Close();
                 networkStream.Close();
             }
             catch (Exception e)
             {
-                Console.WriteLine("Error..... " + e.StackTrace);
+                Console.WriteLine("Error: " + e.StackTrace);
             }
 
             Thread thread = new Thread(new ThreadStart(WorkThreadFunction));
@@ -58,7 +70,7 @@ namespace Client
             }
             catch (Exception e)
             {
-                Console.WriteLine("Error..... " + e.StackTrace);
+                Console.WriteLine("Error: " + e.StackTrace);
             }
         }
 
@@ -92,6 +104,22 @@ namespace Client
         private static void FileSystemWatcher_Deleted(object sender, FileSystemEventArgs e)
         {
             Console.WriteLine("File deleted: {0}", e.Name);
+        }
+
+        private static byte[] Combine(byte[] first, byte[] second)
+        {
+            byte[] bytes = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, bytes, 0, first.Length);
+            Buffer.BlockCopy(second, 0, bytes, first.Length, second.Length);
+            return bytes;
+        }
+
+        private static byte[] AddByteToArray(byte[] bArray, byte newByte)
+        {
+            byte[] newArray = new byte[bArray.Length + 1];
+            bArray.CopyTo(newArray, 1);
+            newArray[0] = newByte;
+            return newArray;
         }
     }
 }
